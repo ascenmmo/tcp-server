edb27ffa895ca8fc7ffe5520e450696250f77f30
temur abdurahmanov <ascemmephone@gmail.com>
2024-10-30 01:17:17 +0300

    add description to env

--- env/env.go
+++ env/env.go
@@ -1,8 +1,8 @@
 package env
 
 var (
-	ServerAddress       = "0.0.0.0"
-	TCPPort             = "8083"
-	TokenKey            = "_remember_token_mast_be_32_bytes"
-	MaxRequestPerSecond = 5
+	ServerAddress       = "0.0.0.0"                          // Server IP address
+	TCPPort             = "8083"                             // Port for TCP connections
+	TokenKey            = "_remember_token_must_be_32_bytes" // Unique token for authentication
+	MaxRequestPerSecond = 5                                  // Max requests per second
 )
